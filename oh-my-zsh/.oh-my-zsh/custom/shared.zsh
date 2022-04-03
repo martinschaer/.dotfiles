c33e57832a6aa2854739a53367c5fe4e44a96768
Martin Schaer <martin@schaerweb.com>
2022-04-02 18:35:14 -0600

    Add menu script

--- oh-my-zsh/.oh-my-zsh/custom/shared.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/shared.zsh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-# echo "This is .oh-my-zsh/custom/shared.zsh speaking"
+function yes_or_no
+{
+  while true; do
+    printf '%s %s ' $1 '[y/n]'
+    read yn
+    case $yn in
+      [Yy]*) return 0 ;;
+      [Nn]*) return 1 ;;
+    esac
+  done
+}
+
+function changeNode14
+{
+  echo -n "nvm current: "
+  nvm current
+  if yes_or_no "run: nvm use 14.18.1"; then
+    nvm use 14.18.1
+  fi
+}
+
+menu ()
+{
+  echo -n $fg[green]
+  echo "\nHello :)${fg[default]}\n"
+  selected=`find $HOME/.config/personal/projects -maxdepth 1 -type f | xargs -L1 basename | fzf --height=10 --reverse --border`
+  if [[ -z $selected ]]; then
+    exit 0
+  fi
+  file=$HOME/.config/personal/projects/$selected
+  printf "You selected %s\n" $file
+  source $file
+}
+
+alias menu="menu"
