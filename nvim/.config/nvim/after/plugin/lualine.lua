74bd89e24cf45dd14544c4f77486065f2b7f99da
Martin Corrales Schaer <m.corrales.schaer@accenture.com>
2022-05-02 17:01:49 -0600

    Add tabline to lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -3,4 +3,12 @@ require('lualine').setup{
     icons_enabled = true,
     theme = 'onedark',
   },
+  tabline = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'buffers'},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {}
+  },
 }
