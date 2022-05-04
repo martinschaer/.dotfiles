8db6b58634264702568fed5a877073e2d9cccf07
Martin Corrales Schaer <m.corrales.schaer@accenture.com>
2022-05-04 12:20:26 -0600

    Add relative filename to lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,12 +1,20 @@
-require('lualine').setup{
+require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'onedark',
   },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { { 'filename', path = 1 } },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
   tabline = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'buffers'},
+    lualine_c = { 'buffers' },
     lualine_x = {},
     lualine_y = {},
     lualine_z = {}
