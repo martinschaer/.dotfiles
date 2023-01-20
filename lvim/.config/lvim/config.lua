8c1aef08449fe53edc93b82555e9269b9c4ca6bc
Martin Schaer <martin@schaerweb.com>
2023-01-20 08:05:31 -0600

    Add prettierd and eslint_d

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -161,23 +161,24 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   --   { command = "black", filetypes = { "python" } },
   --   { command = "isort", filetypes = { "python" } },
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    -- command = "prettierd \"${INPUT}\"",
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact", "astro", "vue", "html" },
-  },
+  -- {
+  --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+  --   -- command = 'prettierd "${INPUT}"',
+  --   command = "prettier",
+  --   ---@usage arguments to pass to the formatter
+  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+  --   extra_args = { "--print-with", "100" },
+  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+  --   filetypes = { "typescript", "typescriptreact", "astro", "vue", "html" },
+  -- },
+  { command = "prettierd" },
 }
 
 -- -- set additional linters
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
   {
-    name = "eslint",
+    name = "eslint_d",
     filetype = {
       "javascript",
       "javascriptreact",
