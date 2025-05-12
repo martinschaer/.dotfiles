4fa99603dcc2f5f0b750d41e8f7e445566450700
Martin Schaer <martin.schaer@surrealdb.com>
2025-05-12 10:46:23 +0100

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    abbr --add lg lazygit
-    abbr --add gco git checkout
     abbr --add gf git fetch
     abbr --add gl git pull
 end
 
 starship init fish | source
 
+source "$HOME/.cargo/env.fish"
